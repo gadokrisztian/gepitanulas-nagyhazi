a31e186a50ee9e4bf53acc683123d0a108c7d0d7
gadokrisztian <gado.krisztian@montana.hu>
2021-11-10 18:56:55 +0100

    comments added

--- whouserobot/projectpaths.py
+++ whouserobot/projectpaths.py
@@ -2,5 +2,9 @@ from pathlib import Path
 
 
 class Dir:
+    """
+    This class holds the paths for quick referencing.
+    """
+
     BASE = Path(__file__).parent.parent
     MEDIA = BASE / "media"
